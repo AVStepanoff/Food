4bdcbbc464014a84c62c929ad47627046042a646
Aleksey <psstudio.info@gmail.com>
2020-12-06 00:53:07 +0300

    Табы и таймер

---
+++ js/script.js
@@ -0,0 +1,118 @@
+    window.addEventListener('DOMContentLoaded', () => {
+
+        // Tabs
+
+        const tabs = document.querySelectorAll('.tabheader__item'),
+            tabsContent = document.querySelectorAll('.tabcontent'),
+            tabsParent = document.querySelector('.tabheader__items');
+
+        //    function hideTabContent() {
+        //        tabsContent.forEach(item => {
+        //             item.style.display = 'none';       
+        //        });
+
+        //        tabs.forEach(item => {
+        //             item.classList.remove('tabheader__item_active');
+        //        });
+        //    }      
+
+        //    function showTabContent(i = 0) {
+        //         tabsContent[i].style.display = 'block';
+        //         tabs[i].classList.add('tabheader__item_active');
+        //    }
+
+        function hideTabContent() {
+            tabsContent.forEach(item => {
+                item.classList.add('hide');
+                item.classList.remove('show', 'fade');
+            });
+
+            tabs.forEach(item => {
+                item.classList.remove('tabheader__item_active');
+            });
+        }
+
+        function showTabContent(i = 0) {
+            tabsContent[i].classList.add('show', 'fade');
+            tabsContent[i].classList.remove('hide');
+            tabs[i].classList.add('tabheader__item_active');
+        }
+
+        hideTabContent();
+        showTabContent(); // вызываем без аргумента, т.к. установлен параметр по умолчанию для i = 0
+
+        tabsParent.addEventListener('click', (event) => {
+            const target = event.target; // упрощаем себе жизнь чтобы было короче писать
+
+            if (target && target.classList.contains('tabheader__item')) {
+                tabs.forEach((item, i) => {
+                    if (target == item) {
+                        hideTabContent();
+                        showTabContent(i);
+                    }
+                });
+            }
+
+        });
+
+        // Timer
+
+        const deadLine = '2020-12-22';
+
+        // функция для получения разницы между датами текущей и дедлайна
+
+        function getTimeRemaining(endtime) {
+            const t = Date.parse(endtime) - Date.parse(new Date()),
+                  days = Math.floor(t/(1000*60*60*24)),
+                  hours = Math.floor((t/1000*60*60)%24),
+                  minutes = Math.floor((t/1000/60)%60),
+                  seconds = Math.floor((t/1000)%60);
+
+            return {
+                'total': t,
+                'days': days,
+                'hours': hours,
+                'minutes': minutes,
+                'seconds': seconds
+            };
+        }
+
+        function getZero(num) {
+            if(num >= 0 && num < 10) {
+                return `0${num}`;
+            } else {
+                return num;
+            }
+        }
+
+        // функция для установки таймера на странице
+
+        function setClock(selector, endtime) {
+            const timer = document.querySelector(selector),
+                  days = timer.querySelector('#days'),
+                  hours = timer.querySelector('#hours'),
+                  minutes = timer.querySelector('#minutes'),
+                  seconds = timer.querySelector('#seconds'),
+                  timeInterval = setInterval(updateClock, 1000);
+
+            updateClock();
+
+
+            function updateClock() {
+                const t = getTimeRemaining(endtime);
+
+                days.innerHTML = getZero(t.days);
+                hours.innerHTML = getZero(t.hours);
+                minutes.innerHTML = getZero(t.minutes);
+                seconds.innerHTML = getZero(t.seconds);
+
+                if(t.total <= 0) {
+                    clearInterval(setInterval);
+                }
+            }
+        }
+
+        setClock('.timer', deadLine);
+
+    });
+
