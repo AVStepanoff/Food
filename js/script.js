804252a1366510e03fe2f76f93093f34f449cf2c
Alex <psstudio.info@gmail.com>
2023-07-08 00:49:49 +0300

    Использование оператора rest и параметров по умолчанию

--- js/script.js
+++ js/script.js
@@ -153,12 +153,13 @@ window.addEventListener('DOMContentLoaded', () => {
     // Используем классы для карточек меню
 
     class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector) {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
+            this.classes = classes;
             this.parent = document.querySelector(parentSelector); // получаем родительский элемент
             this.transfer = 27; // статический курс валют
             this.changeToRUB(); // вызов метода конвертации из новосозданного объекта
@@ -170,19 +171,22 @@ window.addEventListener('DOMContentLoaded', () => {
     
         render() {
             const element = document.createElement('div'); // создаём новый элемент 
-        
-            // далее в этот элемент помещаем вёрстку
-        
+
+            if (this.classes.length === 0) {
+                this.classes = 'menu__item';
+                element.classList.add(this.classes);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+               
             element.innerHTML = `
-                <div class="menu__item">
-                    <img src=${this.src} alt=${this.alt}>
-                    <h3 class="menu__item-subtitle">${this.title}</h3>
-                    <div class="menu__item-descr">${this.descr}</div>
-                    <div class="menu__item-divider"></div>
-                    <div class="menu__item-price">
-                        <div class="menu__item-cost">Цена:</div>
-                        <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
-                    </div>
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
                 </div>
             `;
         
@@ -196,7 +200,8 @@ window.addEventListener('DOMContentLoaded', () => {
         'Меню "Фитнес"',
         'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
         9,
-        '.menu .container'
+        '.menu .container',
+        'menu__item'
     ).render();
     
     new MenuCard(
@@ -205,7 +210,8 @@ window.addEventListener('DOMContentLoaded', () => {
         'Меню “Премиум”',
         'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
         20,
-        '.menu .container'
+        '.menu .container',
+        'menu__item'
     ).render();
     
     new MenuCard(
@@ -214,6 +220,7 @@ window.addEventListener('DOMContentLoaded', () => {
         'Меню "Постное"',
         'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
         16,
-        '.menu .container'
+        '.menu .container',
+        'menu__item'
     ).render();
 });
